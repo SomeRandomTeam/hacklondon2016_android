d633887da0e05967fd50b5f10ff18b9127678bdd
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-02-27 13:21:17 +0000

    Typeface change

--- app/src/main/java/com/somrandomteam/hacklondon2016/MainActivity.java
+++ app/src/main/java/com/somrandomteam/hacklondon2016/MainActivity.java
@@ -1,5 +1,6 @@
 package com.somrandomteam.hacklondon2016;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // New Typeface. Use throughout the app.
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/mob.ttf");
+
+        TextView welcomeText = (TextView) findViewById(R.id.welcometext);
+        welcomeText.setTypeface(customFont);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/somrandomteam/hacklondon2016/tabs/Map.java
@@ -0,0 +1,7 @@
+package com.somrandomteam.hacklondon2016.tabs;
+
+/**
+ * Created by abhinavmishra on 27/02/2016.
+ */
+public class Map {
+}
